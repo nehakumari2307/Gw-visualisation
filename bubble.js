2c0c6d13d85f8dd6f2b7a2c656a756713b1b5599
nehakumari2307 <35798530+nehakumari2307@users.noreply.github.com>
2022-02-13 19:16:39 -0600

    Add files via upload

--- bubble.js
+++ bubble.js
@@ -8,17 +8,42 @@
         .append("g")
         .attr("transform", "translate(0,0)")
 
+    var radiusScale = d3.scaleSqrt().domain([1, 100000]).range([10, 100])
+
+
+    var simulation = d3.forceSimulation()
+        .force("x", d3.forceX(width / 2).strength(0.05))
+        .force("y", d3.forceY(height / 2).strength(0.05))
+        .force("collide", d3.forceCollide(function (d) {
+            return radiusScale(d.area_hectares) + 1;
+        }))
+
+
     d3.queue()
         .defer(d3.csv, "test.csv")
         .await(ready)
 
     function ready(error, datapoints) {
+
         var circles = svg.selectAll(".name")
             .data(datapoints)
             .enter().append("circle")
             .attr("class", "name")
-            .attr("r", 10)
+            .attr("r", function (d) {
+                return radiusScale(d.area_hectares)
+            })
             .attr("fill", "lightblue")
+
+        simulation.nodes(datapoints).on('tick', ticked)
+
+        function ticked() {
+            circles.attr("cx", function (d) {
+                return d.x
+            })
+                .attr("cy", function (d) {
+                    return d.y
+                })
+        }
 
     }
 
