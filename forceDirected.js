2436944d7cd6b8b7c2cf883008de0712c3a806c2
nehakumari2307 <35798530+nehakumari2307@users.noreply.github.com>
2022-02-16 11:09:35 -0600

    Add files via upload

--- forceDirected.js
+++ forceDirected.js
@@ -1,7 +1,5 @@
 (function () {
 
-    import { forceSimulation, forceManyBody, forceLink, forceCenter } from 'd3';
-
     const nodes = [
         { "id": "Alice" },
         { "id": "Bob" },
@@ -13,10 +11,10 @@
         { "source": 1, "target": 2 },  //Bob -> Carol
     ];
 
-    const simulation = forceSimulation(nodes)
-        .force("charge", forceManyBody()) //
-        .force("links", forceLink(links))
-        .force("center", forceCenter()); //pulls nodes towards center
+    const simulation = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody()) //
+        .force("links", d3.forceLink(links))
+        .force("center", d3.forceCenter()); //pulls nodes towards center
 
     simulation.on('tick', () => {
         console.log('tick');
