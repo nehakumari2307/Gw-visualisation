e0f3f7f9b60984c4e73dae5bc8ef78c5bd39add6
nehakumari2307 <35798530+nehakumari2307@users.noreply.github.com>
2022-02-16 20:11:57 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -33,23 +33,39 @@ const addSubNode = (node) => {
     nodes.push(node);
 };
 
-const addChildNode = (subNode, childNode, size = 25, distance = 150 ) => {  //default size for child node
+const addChildNode = (subNode, childNode, size = 25, distance = 100 ) => {  //default size for child node
     childNode.size = size; //const value which can be changed
     nodes.push(childNode);
     links.push({ source: subNode, target: childNode, distance: distance}); //use the node itself as index value of target, source, distance is the distance between 2 nodes
 };
-
-const subNode = { "id": "Art Web"}; //parent node for 2nd screen, 
-addSubNode(subNode); //this will create a subnode
-
-const assembleChildNode = (id) => { 
+//function to connect parent node with childnode ( subnode --> childnode && childnode --> leafnode)
+const assembleChildNode = (subNode, id, articles) => { 
     const childNode ={ id };
     addChildNode(subNode, childNode); //pass the subNode and the childNode //Art Web -> Community
 
-    for(let i=0; i<=20; i++) {
+    for(let i=0; i<=articles; i++) {
         addChildNode(childNode, { "id" : ""}, 10, 50) //pass the childnode abd the leave node //community -> 20 articles with no label, for leave node pass size
     }
 };
 
-assembleChildNode("Community"); //Art web -> Community
-assembleChildNode("Silicon Valley"); //Art Web -> Silicon Valley
+//function to connect subnodes with each other
+const connectSubNodes=(source, target)=> {
+    links.push(source,target,distance: 100)
+}
+
+const artWebNode = { "id": "Art Web"}; //parent node for 2nd screen, 
+addSubNode(artWebNode); //this will create a subnode
+
+assembleChildNode(artWebNode, "Community", 20); //Art web -> Community
+assembleChildNode(artWebNode, "Silicon Valley", 10); //Art Web -> Silicon Valley
+
+const socialImpactNode = { "id": "Social Impact"}; //parent node for 2nd screen, 
+addSubNode(socialImpactNode); //this will create a new subnode
+
+//connect two subNodes together
+connectSubNodes(artWebNode,socialImpactNode);
+
+//add children to second subNode
+assembleChildNode(socialImpactNode, "Art Alliance", 5); //Art web -> Community
+assembleChildNode(socialImpactNode, "Local Color", 7); //Art Web -> Silicon Valley
+assembleChildNode(socialImpactNode, "Threatre", 9); //Art Web -> Silicon Valley
